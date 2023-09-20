a31dc6013a9a6705013308e7d214714964a38b85
Enrique Alonso <alonso.em@gmail.com>
2023-09-20 15:22:28 -0300

    show activities list

--- src/activities.js
+++ src/activities.js
@@ -1,59 +1,60 @@
 import React from 'react';
 import { getActivities } from './api/api.js';
+import { useEffect,useState } from 'react';
 
 
 
 
-export default class ActivitiesBeta extends  React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activities:[],
-        };            
-    }
-
-    componentDidMount(){
-        getActivities()       
-        .then((response) => {
-                this.setState({activities:response.activities});
-                console.log(response.activities);
-        })
-        .catch((response) => {
-            //handleAxiosError(response)
-            console.log(response);
-            }
-        );
-
-    }
-
+function  ActivitiesBeta () {
     
-   
+	const [ activities,setActivities] = useState([]);
+    
+    useEffect(() => {
+            getActivities()       
+            .then((response) => {
+                setActivities(response.activities);
+                    console.log(response.activities);
+            })
+            .catch((response) => {
+                //handleAxiosError(response)
+                console.log(response);
+                }
+            );
+        
+            // eslint-disable-next-line
+        }, []
+    )
 
-       
-    render() {
-
-       
       
         return (
                             
-            <div class="container-fluid">
-                <div class="row">
+            <div class="container ">
+                <div class="row row-cols-3 row-cols-md-3 ">
                 
                     
-                            { this.state.activities.map((each)=>
+                            {activities.map((each)=>
                             {    
                                 return (
-                                    <div class="col m-3">
-                                    <div className="card m-3">
-                                        <div className="card-title">
-                                            <a href={"/activities/"+each.id} > {each.title}</a>
+                                    <div class="col mb-4">
+                                    <div class="card cardActivity" >
+                                         <img class="card-img-top" src="/style/aniversario74.jpg" height="130x" width="400px" alt={each.title} />
+                                         <div class="card-body">
+                                            <h5 class="card-title "><a class="link-underline link-underline-opacity-0 text-body" href={"/activities/"+each.id} >{each.title}</a></h5>
+                                            <p class="card-text">{each.description.substring(1,300)+"..."}</p>
+                                            
                                         </div>
-                                        <div className="card-body">{each.description} </div>
                                     </div>
                                     </div>
+
+                                    
                                     );
                            } 
                             )}
+                            
+                            
+
+
+                    
                     
                 
                 </div>
@@ -62,7 +63,8 @@ export default class ActivitiesBeta extends  React.Component {
         );
 
 
-    }
+    
 
 
 }
+export default ActivitiesBeta
