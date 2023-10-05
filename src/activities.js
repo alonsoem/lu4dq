2122bedc41dc57ce4e68c6daa17509527d790733
Enrique Alonso <alonso.em@gmail.com>
2023-10-05 17:13:54 -0300

    fix imagen dinamica

--- src/activities.js
+++ src/activities.js
@@ -30,14 +30,12 @@ function  ActivitiesBeta () {
                             
             <div class="container ">
                 <div class="row row-cols-3 row-cols-md-3 ">
-                
-                    
                             {activities.map((each)=>
                             {    
                                 return (
                                     <div class="col mb-4">
                                     <div class="card cardActivity" >
-                                         <img class="card-img-top" src="/style/aniversario74.jpg" height="130x" width="400px" alt={each.title} />
+                                         <img class="card-img-top" src={"/style/"+each.image} height="130x" width="400px" alt={each.title} />
                                          <div class="card-body">
                                             <h5 class="card-title "><a class="link-underline link-underline-opacity-0 text-body" href={"/activities/"+each.id} >{each.title}</a></h5>
                                             <p class="card-text">{each.description.substring(1,300)+"..."}</p>
