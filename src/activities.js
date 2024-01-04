ddde6ddcf10ac96a4d1772e93574c21b7bfc641f
Enrique Alonso <alonso.em@gmail.com>
2024-01-04 17:30:47 -0300

    fix default image

--- src/activities.js
+++ src/activities.js
@@ -38,7 +38,7 @@ function  ActivitiesBeta () {
                                 return (
                                     <div class="col mb-4">
                                     <div class="card cardActivity" >
-                                         <img class="card-img-top" src={"/dinamic-content/FRT/"+each.image} height="130x" width="400px" alt={each.title} />
+                                         <img class="card-img-top" src={(each.image?"/dinamic-content/FRT/"+each.image:"/static/defaultFrontImage.jpg")} height="130x" width="400px" alt={each.title} />
                                          <div class="card-body">
                                             <h5 class="card-title "><a class="link-underline link-underline-opacity-0 text-body" href={"/activities/"+each.id} >{each.title}</a></h5>
                                             <p class="card-text"> {Parser().parse(DOMPurify.sanitize(each.description.substring(0,300)).toString()+"...")}</p>
