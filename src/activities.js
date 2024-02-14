52664e26b1207b1b327f0c208f9fdde52e4b91b3
Enrique Alonso <alonso.em@gmail.com>
2024-02-14 09:52:35 -0300

    fix message with no activities

--- src/activities.js
+++ src/activities.js
@@ -3,7 +3,7 @@ import { getActivities } from './api/api.js';
 import { useEffect,useState } from 'react';
 import * as DOMPurify from 'dompurify';
 import {Parser} from "html-to-react";
-
+import TimeUp from './timeUp.js';
 
 
 
@@ -33,32 +33,26 @@ function  ActivitiesBeta () {
                             
             <div class="container ">
                 <div class="row row-cols-3 row-cols-md-3 ">
-                            {activities.map((each)=>
-                            {    
-                                return (
-                                    <div class="col mb-4">
-                                    <div class="card cardActivity" >
-                                         <img class="card-img-top" src={(each.image?"/dinamic-content/FRT/"+each.image:"/static/defaultFrontImage.jpg")} height="130x" width="400px" alt={each.title} />
-                                         <div class="card-body">
-                                            <h5 class="card-title "><a class="link-underline link-underline-opacity-0 text-body" href={"/activities/"+each.id} >{each.title}</a></h5>
-                                            <p class="card-text"> {Parser().parse(DOMPurify.sanitize(each.description.substring(0,300)).toString()+"...")}</p>
-                                            
-                                        </div>
-                                    </div>
-                                    </div>
-
+                    {activities.length>0 && activities.map((each)=>
+                    {    
+                        return (
+                            <div class="col mb-4">
+                            <div class="card cardActivity" >
+                                    <img class="card-img-top" src={(each.image?"/dinamic-content/FRT/"+each.image:"/static/defaultFrontImage.jpg")} height="130x" width="400px" alt={each.title} />
+                                    <div class="card-body">
+                                    <h5 class="card-title "><a class="link-underline link-underline-opacity-0 text-body" href={"/activities/"+each.id} >{each.title}</a></h5>
+                                    <p class="card-text"> {Parser().parse(DOMPurify.sanitize(each.description.substring(0,300)).toString()+"...")}</p>
                                     
-                                    );
-                           } 
-                            )}
-                            
-                            
+                                </div>
+                            </div>
+                            </div>
 
-
-                    
-                    
-                
+                            
+                            );
+                    } 
+                    )}
                 </div>
+                {activities.length===0?<TimeUp />:null}
             </div>
 
         );
--- src/timeUp.js
+++ src/timeUp.js
@@ -6,10 +6,7 @@ function TimeUp(){
                       <div class="container object-fit-sm-contain border rounded bg-white p-5" >
                           <img src="/style/timesup.jpg" class="img-fluid " alt="NO HAY ACTIVIDADES DISPONIBLES ACTUALMENTE"/>
                           <p class="m-4 p-4 fs-3">¡NO HAY ACTIVIDADES DISPONIBLES ACTUALMENTE!</p>
-                          <p class="m-1">
-                              <a class="btn btn-success" href="/activities">Ver actividades</a>
-                              
-                          </p>
+                          
                       </div>
                       
                   </div>
