09a6ef0d0f6b902facdbc85688e31a8de1348023
Enrique Alonso <alonso.em@gmail.com>
2024-04-05 20:38:42 -0300

    hamburguer menu auto hiden options

--- src/nav.js
+++ src/nav.js
@@ -1,30 +1,35 @@
 import 'react-bootstrap-typeahead/css/Typeahead.css';
-import React from "react";
+import {React,useState} from "react";
+
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {Container} from 'react-bootstrap';
 
 
 //import "react-datepicker/dist/react-datepicker.css";
 //import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 function NavMenu() {
+
+  const [expanded, setExpanded] = useState(false);
+
     return (
-      <div class="">
-        <Navbar collapseOnSelect  className="p-3 navbar nav navbar-dark bg-dark navmenu navLu4dq justify-content-center" expand="sm">
-          
-                
+      <div class="justify-content-center">
+        <Navbar   className="p-3 navbar nav navbar-dark bg-dark navmenu navLu4dq justify-content-center" expand="sm"  >
+          <Container>
             
-    <Navbar.Toggle aria-controls="responsive-navbar-nav " class="navbar-toggle" />
-      <Navbar.Collapse id="responsive-navbar-nav " class="navbar-toggle">
-        <Nav className="nav  ms-4">
-              <Nav.Link class="navlink" href="/activities">ACTIVIDADES</Nav.Link>
-             
-              <Nav.Link class="navlink" href="/">CARGA DE CONTACTOS</Nav.Link>  
-          
-              <Nav.Link class="navlink" href="/qsoList">VER CONTACTOS</Nav.Link>
-        </Nav>
-          </Navbar.Collapse>
-    </Navbar>
+            <Navbar.Toggle  aria-controls="responsive-navbar-nav"  />
+            <Navbar.Collapse id="responsive-navbar-nav " >
+              <Nav className="me-auto">
+                    <Nav.Link class="navlink" href="/activities">ACTIVIDADES</Nav.Link>
+                  
+                    <Nav.Link class="navlink" href="/">CARGA DE CONTACTOS</Nav.Link>  
+                
+                    <Nav.Link class="navlink" href="/qsoList">VER CONTACTOS</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
 
     );
