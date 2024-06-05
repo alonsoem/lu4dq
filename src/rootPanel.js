f17f35edd6cf08bb370073bbb0881316aaf132d4
ENRIQUE ALONSO <alonso.em@gmail.com>
2024-06-05 19:19:47 -0300

    fix root panel

--- src/rootPanel.js
+++ src/rootPanel.js
@@ -34,7 +34,7 @@ export default class App extends React.Component {
     render() {
       return (
         
-        <BrowserRouter basename='/wp/logs' >
+        <BrowserRouter  >
         <NavMenu />
           <Routes>
             
