238bc19346aec7132857c9f1be5e91fdda2562d8
ENRIQUE ALONSO <alonso.em@gmail.com>
2024-08-23 22:18:41 -0300

    fix response status

--- src/upload2.js
+++ src/upload2.js
@@ -269,18 +269,18 @@ function Upload(){
 
 		const showBadgeMov = (reg) => {
 			// eslint-disable-next-line
-			if (reg.insert=="OK"){
+			if (reg.response=="OK"){
 				return <span class="badge bg-success">Aceptado</span>
 			// eslint-disable-next-line
-			}else if (reg.insert=="duplicate"){
+			}else if (reg.response=="duplicate"){
 				return <span class="badge bg-warning">{reg.insert}</span>
 			// eslint-disable-next-line
-			}else if (reg.insert=="No Match Station"){
+			}else if (reg.response=="No Match Station"){
 				return <span class="badge bg-danger" data-toggle="tooltip" data-placement="right" data-title={reg.message} >
 							{reg.message}
 							</span>	
 			// eslint-disable-next-line
-			}else if (reg.insert=="Self Qso"){
+			}else if (reg.response=="Self Qso"){
 				return <span class="badge bg-danger" data-toggle="tooltip" data-placement="right" data-title={reg.message} >
 					{reg.message}
 					</span>							
